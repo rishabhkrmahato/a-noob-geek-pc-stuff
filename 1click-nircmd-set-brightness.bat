76b2580307c86e3a1aab5e6bcf953785f442ed9b
rishabhkrm <mahato.rishabhkr60@gmail.com>
2024-09-03 16:28:56 +0530

    updated files in main
    restructured main branch

---
+++ 1click-nircmd-set-brightness.bat
@@ -0,0 +1,9 @@
+@echo off
+
+:: windows does not have native brightness control command line utility
+:: get nircmd from nirsoft, its a multipurpose great cmd utility
+:: add it to path or same system folder (faster way): unzip the contents and move nircmd.exe and nircmdc.exe to "C:\Windows\System32"
+
+nircmd.exe setbrightness 75 ::change this to your desired value and rename the file, eg. set brightness to 75%.bat
+
+
