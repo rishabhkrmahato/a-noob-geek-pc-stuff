2bddb7bf0aab8aa7db00550457a7b8344bef9904
rishabhkrm <mahato.rishabhkr60@gmail.com>
2024-10-06 22:35:57 +0530

    Update end-task-valorant.bat

--- bat/end-task-valorant.bat
+++ bat/end-task-valorant.bat
@@ -1,11 +1,19 @@
 @echo off
+:: Check if the script is running as administrator, if not, relaunch as admin
+:: Batch code to request administrative privileges
+net session >nul 2>&1
+if %errorLevel% neq 0 (
+    echo Requesting admin privileges...
+    powershell -Command "Start-Process '%~f0' -Verb runAs"
+    exit /B
+)
 
-:: simple batch, just to completely end valorant after done gaming, cause sometimes it runs in the background even after ending.
-
-echo Stopping RiotClient and Valorant processes...
+:: Now that we are running as admin, proceed with the task killing
+echo Stopping RiotClient, Valorant, Overwolf, and Valorant Tracker processes...
+echo.
+echo.
 
 :: Taskkill for Riot and Valorant related processes along with their child processes
-
 taskkill /F /T /IM RiotClientServices.exe
 taskkill /F /T /IM RiotClientUx.exe
 taskkill /F /T /IM RiotClientUxRender.exe
@@ -14,5 +22,11 @@ taskkill /F /T /IM VanguardTray.exe
 taskkill /F /T /IM vgtray.exe
 taskkill /F /T /IM vgc.exe
 
+:: Taskkill for Overwolf and Valorant Tracker processes along with their child processes
+taskkill /F /T /IM "Valorant Tracker.exe"
+taskkill /F /T /IM Overwolf.exe
+
+echo.
+echo.
 echo All processes terminated, including child processes.
 pause
