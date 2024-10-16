aafe15ba5fdb2d29c925d40cdbb4ae7163542192
rishabhkrm <mahato.rishabhkr60@gmail.com>
2024-10-16 17:52:00 +0530

    Update update-choco-scoop-pip.bat

--- bat/update-choco-scoop-pip.bat
+++ bat/update-choco-scoop-pip.bat
@@ -1,18 +1,34 @@
 @echo off
-:: Script to update all packages for Chocolatey, Scoop, and pip
 
+:: Check if the script is running as administrator, if not, relaunch as admin
+net session >nul 2>&1
+if %errorLevel% neq 0 (
+    echo Requesting admin privileges...
+    powershell -Command "Start-Process '%~f0' -Verb runAs"
+    exit /B
+)
+
+:: Script to update all packages for Chocolatey, Scoop, and pip
 echo Updating Chocolatey packages...
 choco upgrade all --yes
 
 echo.
 echo Updating Scoop packages...
-scoop update * --force
+scoop update --force
 
 echo.
 echo Updating pip packages...
+:: Use the pip freeze command correctly and filter out unwanted packages
 pip list --outdated --format=freeze | findstr /v "pip==" | findstr /v "setuptools==" > outdated.txt
-for /f "delims==" %%i in (outdated.txt) do pip install --upgrade %%i
-del outdated.txt
+if exist outdated.txt (
+    for /f "delims==" %%i in (outdated.txt) do (
+        echo Updating %%i...
+        pip install --upgrade %%i --upgrade-strategy only-if-needed
+    )
+    del outdated.txt
+) else (
+    echo No outdated pip packages found.
+)
 
 echo.
 echo All updates are complete!
