3407357ad69386c846f256014be50a91fab4cd22
rishabhkrm <mahato.rishabhkr60@gmail.com>
2024-11-06 10:30:37 +0530

    Create git-commit-changes.bat

---
+++ .bat/git-commit-changes.bat
@@ -0,0 +1,19 @@
+@echo off
+:: Ask for commit message
+set /p commit_msg=Enter your commit message: 
+
+:: Navigate to your repository folder
+cd "C:\Users\mahat\Documents\GitHub\a-noob-geek-pc-stuff"
+:: ***change this to your own "repo folder" location***
+
+:: Stage all changes
+git add .
+
+:: Commit changes with the message
+git commit -m "%commit_msg%"
+
+:: Push changes to the 'main' branch
+git push origin main
+
+echo Commit and push completed successfully!
+pause
