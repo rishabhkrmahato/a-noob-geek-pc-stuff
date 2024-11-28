8a1edd30656f69d4a7da4be954e91e6a84cb5fe2
rishabhkrm <mahato.rishabhkr60@gmail.com>
2024-11-28 09:30:05 +0530

    Update real-time-mouse-co-ord.py

--- py/real-time-mouse-co-ord.py
+++ py/real-time-mouse-co-ord.py
@@ -1,3 +1,6 @@
+# pip install pyautogui
+# install the module first on your pc !
+
 import pyautogui
 import time
 
@@ -6,6 +9,6 @@ try:
         # Get the current position of the mouse
         x, y = pyautogui.position()
         print(f"Mouse Position: X={x}, Y={y}", end="\r")  # Print on the same line
-        time.sleep(0.8)  # change this delay accordingly 
+        time.sleep(0.8)  # change this delay accordingly, done to avoid spamming.
 except KeyboardInterrupt:
-    print("\nProgram exited!") #Ctrl-C or Close terminal to end !
+    print("\nProgram exited!") #Ctrl-C or Close terminal to end.
