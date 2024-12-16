7009883f92e3a5550a03897ace31639a950f65d9
rishabhkrm <mahato.rishabhkr60@gmail.com>
2024-12-16 16:51:17 +0530

    Update rkm-tools.ps1

--- rkm-tools.ps1
+++ rkm-tools.ps1
@@ -106,13 +106,36 @@ do {
             {
                 "1"
                 {
-
+                    $url = "https://raw.githubusercontent.com/rishabhkrmahato/a-noob-geek-pc-stuff/refs/heads/main/.bat/network-refresh.bat"
+                    $scriptPath = "$directoryPath\network-refresh.bat"
+                    Invoke-WebRequest -Uri $url -OutFile $scriptPath
+                    if (Test-Path $scriptPath) {
+                        & $scriptPath
+                    } else {
+                        Write-Host "Failed to download the script." -ForegroundColor Red
+                    }
                 }
                 "2"
                 {
+                    $url = "https://raw.githubusercontent.com/rishabhkrmahato/a-noob-geek-pc-stuff/refs/heads/main/.bat/toggle-ethernet-admin.bat"
+                    $scriptPath = "$directoryPath\toggle-ethernet-admin.bat"
+                    Invoke-WebRequest -Uri $url -OutFile $scriptPath
+                    if (Test-Path $scriptPath) {
+                        & $scriptPath
+                    } else {
+                        Write-Host "Failed to download the script." -ForegroundColor Red
+                    }
                 }
                 "3"
                 {
+                    $url = "https://raw.githubusercontent.com/rishabhkrmahato/a-noob-geek-pc-stuff/refs/heads/main/.bat/nircmd-set-brightness.bat"
+                    $scriptPath = "$directoryPath\nircmd-set-brightness.bat"
+                    Invoke-WebRequest -Uri $url -OutFile $scriptPath
+                    if (Test-Path $scriptPath) {
+                        & $scriptPath
+                    } else {
+                        Write-Host "Failed to download the script." -ForegroundColor Red
+                    }
                 }
                 "4"
                 {
@@ -140,51 +163,172 @@ do {
                 }
                 "5"
                 {
+                    $url = "https://raw.githubusercontent.com/rishabhkrmahato/a-noob-geek-pc-stuff/refs/heads/main/.bat/convert-mkv-to-mp4.bat"
+                    $scriptPath = "$directoryPath\convert-mkv-to-mp4.bat"
+                    Invoke-WebRequest -Uri $url -OutFile $scriptPath
+                    if (Test-Path $scriptPath) {
+                        & $scriptPath
+                    } else {
+                        Write-Host "Failed to download the script." -ForegroundColor Red
+                    }
                 }
                 "6"
                 {
+                    $url = "https://raw.githubusercontent.com/rishabhkrmahato/a-noob-geek-pc-stuff/refs/heads/main/.bat/list-imp-files.bat"
+                    $scriptPath = "$directoryPath\list-imp-files.bat"
+                    Invoke-WebRequest -Uri $url -OutFile $scriptPath
+                    if (Test-Path $scriptPath) {
+                        & $scriptPath
+                    } else {
+                        Write-Host "Failed to download the script." -ForegroundColor Red
+                    }
                 }
                 "7"
                 {
+                    $url = "https://raw.githubusercontent.com/rishabhkrmahato/a-noob-geek-pc-stuff/refs/heads/main/.bat/1click-nircmd-set-brightness.bat"
+                    $scriptPath = "$directoryPath\1click-nircmd-set-brightness.bat"
+                    Invoke-WebRequest -Uri $url -OutFile $scriptPath
+                    if (Test-Path $scriptPath) {
+                        & $scriptPath
+                    } else {
+                        Write-Host "Failed to download the script." -ForegroundColor Red
+                    }
                 }
                 "8"
                 {
+                    $url = "https://raw.githubusercontent.com/rishabhkrmahato/a-noob-geek-pc-stuff/refs/heads/main/.bat/yt-dlp_downloader.bat"
+                    $scriptPath = "$directoryPath\yt-dlp_downloader.bat"
+                    Invoke-WebRequest -Uri $url -OutFile $scriptPath
+                    if (Test-Path $scriptPath) {
+                        & $scriptPath
+                    } else {
+                        Write-Host "Failed to download the script." -ForegroundColor Red
+                    }
                 }
                 "9"
                 {
+                    $url = "https://raw.githubusercontent.com/rishabhkrmahato/a-noob-geek-pc-stuff/refs/heads/main/.bat/split-video-by-mins-lossless.bat"
+                    $scriptPath = "$directoryPath\split-video-by-mins-lossless.bat"
+                    Invoke-WebRequest -Uri $url -OutFile $scriptPath
+                    if (Test-Path $scriptPath) {
+                        & $scriptPath
+                    } else {
+                        Write-Host "Failed to download the script." -ForegroundColor Red
+                    }
                 }
                 "10"
                 {
+                    $url = "https://raw.githubusercontent.com/rishabhkrmahato/a-noob-geek-pc-stuff/refs/heads/main/.bat/open-chatgpt-replace-copilot_key.bat"
+                    $scriptPath = "$directoryPath\open-chatgpt-replace-copilot_key.bat"
+                    Invoke-WebRequest -Uri $url -OutFile $scriptPath
+                    if (Test-Path $scriptPath) {
+                        & $scriptPath
+                    } else {
+                        Write-Host "Failed to download the script." -ForegroundColor Red
+                    }
                 }
                 "11"
                 {
+                    $url = "https://raw.githubusercontent.com/rishabhkrmahato/a-noob-geek-pc-stuff/refs/heads/main/.bat/create-file.bat"
+                    $scriptPath = "$directoryPath\create-file.bat"
+                    Invoke-WebRequest -Uri $url -OutFile $scriptPath
+                    if (Test-Path $scriptPath) {
+                        & $scriptPath
+                    } else {
+                        Write-Host "Failed to download the script." -ForegroundColor Red
+                    }
                 }
                 "12"
                 {
+                    $url = "https://raw.githubusercontent.com/rishabhkrmahato/a-noob-geek-pc-stuff/refs/heads/main/.bat/disable-recall.bat"
+                    $scriptPath = "$directoryPath\disable-recall.bat"
+                    Invoke-WebRequest -Uri $url -OutFile $scriptPath
+                    if (Test-Path $scriptPath) {
+                        & $scriptPath
+                    } else {
+                        Write-Host "Failed to download the script." -ForegroundColor Red
+                    }
                 }
                 "13"
                 {
+                    $url = "https://raw.githubusercontent.com/rishabhkrmahato/a-noob-geek-pc-stuff/refs/heads/main/.bat/update-choco-scoop-pip-npm-installs.bat"
+                    $scriptPath = "$directoryPath\update-choco-scoop-pip-npm-installs.bat"
+                    Invoke-WebRequest -Uri $url -OutFile $scriptPath
+                    if (Test-Path $scriptPath) {
+                        & $scriptPath
+                    } else {
+                        Write-Host "Failed to download the script." -ForegroundColor Red
+                    }
                 }
                 "14"
                 {
+                    $url = "https://raw.githubusercontent.com/rishabhkrmahato/a-noob-geek-pc-stuff/refs/heads/main/.bat/git-commit-all-changes.bat"
+                    $scriptPath = "$directoryPath\git-commit-all-changes.bat"
+                    Invoke-WebRequest -Uri $url -OutFile $scriptPath
+                    if (Test-Path $scriptPath) {
+                        & $scriptPath
+                    } else {
+                        Write-Host "Failed to download the script." -ForegroundColor Red
+                    }
                 }
                 "15"
                 {
+                    $url = "https://raw.githubusercontent.com/rishabhkrmahato/a-noob-geek-pc-stuff/refs/heads/main/.bat/list-all-files-full-paths.bat"
+                    $scriptPath = "$directoryPath\list-all-files-full-paths.bat"
+                    Invoke-WebRequest -Uri $url -OutFile $scriptPath
+                    if (Test-Path $scriptPath) {
+                        & $scriptPath
+                    } else {
+                        Write-Host "Failed to download the script." -ForegroundColor Red
+                    }
                 }
                 "16"
                 {
+                    $url = "https://raw.githubusercontent.com/rishabhkrmahato/a-noob-geek-pc-stuff/refs/heads/main/.bat/clear-bin-and-temp.bat"
+                    $scriptPath = "$directoryPath\clear-bin-and-temp.bat"
+                    Invoke-WebRequest -Uri $url -OutFile $scriptPath
+                    if (Test-Path $scriptPath) {
+                        & $scriptPath
+                    } else {
+                        Write-Host "Failed to download the script." -ForegroundColor Red
+                    }
                 }
                 "17"
                 {
+                    $url = "https://raw.githubusercontent.com/rishabhkrmahato/a-noob-geek-pc-stuff/refs/heads/main/.bat/aio-package-updater.bat"
+                    $scriptPath = "$directoryPath\aio-package-updater.bat"
+                    Invoke-WebRequest -Uri $url -OutFile $scriptPath
+                    if (Test-Path $scriptPath) {
+                        & $scriptPath
+                    } else {
+                        Write-Host "Failed to download the script." -ForegroundColor Red
+                    }
                 }
                 "18"
                 {
+                    $url = "https://raw.githubusercontent.com/rishabhkrmahato/a-noob-geek-pc-stuff/refs/heads/main/.bat/take-backup-compress-copy.bat"
+                    $scriptPath = "$directoryPath\take-backup-compress-copy.bat"
+                    Invoke-WebRequest -Uri $url -OutFile $scriptPath
+                    if (Test-Path $scriptPath) {
+                        & $scriptPath
+                    } else {
+                        Write-Host "Failed to download the script." -ForegroundColor Red
+                    }
                 }
                 "19"
                 {
+                    $url = "https://raw.githubusercontent.com/rishabhkrmahato/a-noob-geek-pc-stuff/refs/heads/main/.bat/1click-reg-backup.bat"
+                    $scriptPath = "$directoryPath\1click-reg-backup.bat"
+                    Invoke-WebRequest -Uri $url -OutFile $scriptPath
+                    if (Test-Path $scriptPath) {
+                        & $scriptPath
+                    } else {
+                        Write-Host "Failed to download the script." -ForegroundColor Red
+                    }
                 }
                 "0"
                 {
+                    break
                 }
                 Default
                 {
@@ -192,7 +336,6 @@ do {
                     Write-Host ""
                 }
             }
-
         }
         "2" 
         {    
@@ -201,7 +344,7 @@ do {
             Write-Host ""
             Write-Host "[1] install-must-have-pkgs.sh"
             Write-Host "[2] termux-install-must-have-pkgs.sh"
-            Write-Host "[3] yt-dlp+termux-downloader-android.sh"
+            Write-Host "[3] yt-dlp-termux-downloader-android.sh"
             Write-Host ""
             Write-Host "[0] Back to Main Menu"
             Write-Host ""
@@ -214,12 +357,44 @@ do {
             {
                 "1"
                 {
+                    # Define the URL and the save path
+                    $url = "https://raw.githubusercontent.com/rishabhkrmahato/a-noob-geek-pc-stuff/refs/heads/main/bash/install-must-have-pkgs.sh"
+                    $scriptPath = "$directoryPath\install-must-have-pkgs.sh"
+
+                    # Download the file from the URL and save it to the specified path
+                    Invoke-WebRequest -Uri $url -OutFile $scriptPath
+
+                    # Notify the user about the saved script
+                    if (Test-Path $scriptPath) {
+                        Write-Host "Script saved to: $scriptPath" -ForegroundColor Green
+                        Write-Host "Note: Use this script on a compatible OS, such as Linux."
+                    } else {
+                        Write-Host "Failed to download the script." -ForegroundColor Red
+                    }
                 }
                 "2"
                 {
+                    $url = "https://raw.githubusercontent.com/rishabhkrmahato/a-noob-geek-pc-stuff/refs/heads/main/bash/termux-install-must-have-pkgs.sh"
+                    $scriptPath = "$directoryPath\termux-install-must-have-pkgs.sh"
+                    Invoke-WebRequest -Uri $url -OutFile $scriptPath
+                    if (Test-Path $scriptPath) {
+                        Write-Host "Script saved to: $scriptPath" -ForegroundColor Green
+                        Write-Host "Note: Use this script on a compatible env./OS, such as Termux, Linux."
+                    } else {
+                        Write-Host "Failed to download the script." -ForegroundColor Red
+                    }
                 }
                 "3"
                 {
+                    $url = ""
+                    $scriptPath = "$directoryPath\"
+                    Invoke-WebRequest -Uri $url -OutFile $scriptPath
+                    if (Test-Path $scriptPath) {
+                        Write-Host "Script saved to: $scriptPath" -ForegroundColor Green
+                        Write-Host "Note: Use this script on a compatible env./OS, such as Termux, Linux."
+                    } else {
+                        Write-Host "Failed to download the script." -ForegroundColor Red
+                    }
                 }
                 "0"
                 {
