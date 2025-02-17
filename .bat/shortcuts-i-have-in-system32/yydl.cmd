3bdd922d815d5a258dc4e85da7c5214f007e08c5
rishabhkrm <mahato.rishabhkr60@gmail.com>
2025-02-17 10:07:02 +0530

    Update yydl.cmd

--- .bat/shortcuts-i-have-in-system32/yydl.cmd
+++ .bat/shortcuts-i-have-in-system32/yydl.cmd
@@ -1,3 +1,3 @@
 @echo off
 python "C:\WINDOWS\system32\yydl.py" %1
-@REM yydl.py is nothing but,   a-noob-geek-pc-stuff\py\best-yt-dlp-automated.py   only...renamed for easy recall in cmd 
+@REM yydl.py is nothing but,   a-noob-geek-pc-stuff/py/BEST-YOUTUBE-DOWNLOADER.py   only...renamed for easy recall in cmd 
