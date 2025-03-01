7ce6eb4f3cda1c6d1e7acbffc8220fe53d13c659
rishabhkrm <mahato.rishabhkr60@gmail.com>
2025-03-01 22:21:25 +0530

    Update end-task-edge.bat

--- .bat/end-task-edge.bat
+++ .bat/end-task-edge.bat
@@ -1,6 +1,24 @@
-:: edge runs in the background with upto 15 processes, even without signed in, and run edge in background turned off in settings
+:: ================================================================================================
+:: Description:
+:: Force Close Microsoft Edge
+::
+:: This script forcefully terminates all running Microsoft Edge processes, 
+:: even when background activity is disabled in settings.
+::
+:: Usage:
+:: - Run the script to close all Edge instances.
+::
+:: Output:
+:: - Displays confirmation after closing Edge.
+::
+:: Notes:
+:: - Edge may restart if set as the default browser with background tasks enabled.
+:: ================================================================================================
+
 
 @echo off
+:: edge runs in the background with upto 15 processes, even without signed in, and run edge in background turned off in settings
+
 taskkill /F /IM msedge.exe
 echo Microsoft Edge has been completely closed.
 pause
