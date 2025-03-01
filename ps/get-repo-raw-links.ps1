b0bd7fa46ef127895523a4415da1180472e2d6b4
rishabhkrm <mahato.rishabhkr60@gmail.com>
2025-03-01 22:16:03 +0530

    Update get-repo-raw-links.ps1

--- ps/get-repo-raw-links.ps1
+++ ps/get-repo-raw-links.ps1
@@ -1,7 +1,5 @@
 # ================================================================================================
-#
 # Description:
-#
 # Generate Raw GitHub File URLs
 #
 # This PowerShell script generates a list of raw file URLs for all files 
@@ -47,7 +45,6 @@
 #   or repositories.
 # ================================================================================================
 
-# Set your specific details
 
 $username = "rishabhkrmahato"
 $repo = "a-noob-geek-pc-stuff"
@@ -55,19 +52,13 @@ $branch = "main"
 $localRepoPath = "C:\Users\mahat\Documents\GitHub\a-noob-geek-pc-stuff"
 $outputFile = "C:\Users\mahat\Documents\GitHub\rkmtoolslist.txt"
 
-# Navigate to the repository folder
 Set-Location -Path $localRepoPath
 
-# Get all files recursively in the repo
 $files = Get-ChildItem -Recurse -File
 
-# Loop through each file to generate the raw URL
 foreach ($file in $files) {
-    # Construct the relative path and create the raw URL
     $relativePath = $file.FullName.Substring($localRepoPath.Length + 1).Replace("\", "/")
     $rawUrl = "https://raw.githubusercontent.com/$username/$repo/refs/heads/$branch/$relativePath"
-    
-    # Write each file name and its raw URL to the output file
     "$($file.Name): $rawUrl" | Out-File -Append $outputFile
 }
 
