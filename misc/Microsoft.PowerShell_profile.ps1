85c69ef49df5076c62d03aa77289b202c29bed39
rishabhkrm <mahato.rishabhkr60@gmail.com>
2025-03-23 08:21:49 +0530

    Update Microsoft.PowerShell_profile.ps1

--- misc/Microsoft.PowerShell_profile.ps1
+++ misc/Microsoft.PowerShell_profile.ps1
@@ -1,4 +1,8 @@
-# Override default PowerShell and pwsh commands to always use -NoLogo
+# I have symlinked this to:
+# "C:\Users\rishabhkrm\Documents\WindowsPowerShell\Microsoft.PowerShell_profile.ps1"
+# "C:\Users\rishabhkrm\Documents\PowerShell\Microsoft.PowerShell_profile.ps1"
+# also using pws.cmd and pws7.cmd in PATH to open with -NoLogo
+
 function powershell { & "C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe" -NoLogo @args }
 function pwsh { & "C:\Program Files\PowerShell\7\pwsh.exe" -NoLogo @args }
 
