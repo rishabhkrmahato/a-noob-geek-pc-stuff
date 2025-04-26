dcc26a7f3c38e98d88746d90d1918ba7910453bb
rishabhkrm <mahato.rishabhkr60@gmail.com>
2025-04-26 16:38:54 +0530

    update gtn.cmd & removed 2 files

--- .bat/shortcuts-i-have-in-system32/focus-terminal.cmd
+++
@@ -1,4 +0,0 @@
-@echo off
-powershell -NoProfile -ExecutionPolicy Bypass -WindowStyle Hidden -File "C:\WINDOWS\system32\focus-terminal.ps1"
-
-
--- .bat/shortcuts-i-have-in-system32/gtn.cmd
+++ .bat/shortcuts-i-have-in-system32/gtn.cmd
@@ -1,7 +1,25 @@
 @echo off
+setlocal
+
+:: Find MPC-HC (checks both 32-bit and 64-bit locations)
+for %%P in ("%ProgramFiles%\K-Lite Codec Pack\MPC-HC64\mpc-hc64.exe" "%ProgramFiles%\K-Lite Codec Pack\MPC-HC\mpc-hc.exe" "%ProgramFiles(x86)%\K-Lite Codec Pack\MPC-HC64\mpc-hc64.exe" "%ProgramFiles(x86)%\K-Lite Codec Pack\MPC-HC\mpc-hc.exe") do (
+    if exist "%%~P" set "MPC_PATH=%%~P"
+)
+
+:: If not found, show error and exit
+if not defined MPC_PATH (
+    echo Error: MPC-HC not found. Please install K-Lite Mega Codec Pack. 
+    echo Download: https://codecguide.com/download_k-lite_codec_pack_mega.htm
+    pause
+    exit /b
+)
+
+:: Process each file passed as an argument
 for %%i in (%*) do (
-	echo.
+    echo.
     echo Generating thumbnails for "%%~i"
-    "C:\Program Files (x86)\K-Lite Codec Pack\MPC-HC\mpc-hc.exe" /thumbnails "%%~i"
+    "%MPC_PATH%" /thumbnails "%%~i"
     echo Done with "%%~i"
 )
+
+endlocal
