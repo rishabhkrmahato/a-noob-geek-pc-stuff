c3d2b62fc455f8da17d432ad916a4c7b172e8a5b
rishabhkrm <mahato.rishabhkr60@gmail.com>
2025-04-26 16:21:20 +0530

    Update cdgh.cmd

--- .bat/shortcuts-i-have-in-system32/cdgh.cmd
+++ .bat/shortcuts-i-have-in-system32/cdgh.cmd
@@ -1,2 +1,10 @@
+:: ==========================================================================================
+:: Quick CD to GitHub Repo - Personal Script for Rishabh
+::
+:: [Note]
+:: - Designed for Rishabh's PC setup
+:: - May not work for others
+:: ==========================================================================================
+
 @echo off
-cd /d "C:\Users\mahat\Documents\GitHub\a-noob-geek-pc-stuff"
+cd /d "%USERPROFILE%\Documents\GitHub\a-noob-geek-pc-stuff"
