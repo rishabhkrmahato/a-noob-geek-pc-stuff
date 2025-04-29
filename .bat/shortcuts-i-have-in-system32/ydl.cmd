a252e76a6677e9f98dc86d06c305acb830566bfc
rishabhkrm <mahato.rishabhkr60@gmail.com>
2025-04-29 13:35:47 +0530

    update ydl.cmd

--- .bat/shortcuts-i-have-in-system32/ydl.cmd
+++ .bat/shortcuts-i-have-in-system32/ydl.cmd
@@ -1,2 +1,9 @@
 @echo off
-yt-dlp -f bestvideo+bestaudio -o "C:\Users\mahat\Videos\%%(title)s.%%(ext)s" %1
+set SCRIPT_URL="https://raw.githubusercontent.com/rishabhkrmahato/a-noob-geek-pc-stuff/main/py/BEST-YOUTUBE-DOWNLOADER.py"
+set TEMP_SCRIPT_PATH="%TEMP%\temp-BEST-YOUTUBE-DOWNLOADER.py"
+powershell -NoProfile -ExecutionPolicy Bypass -Command "(Invoke-WebRequest -Uri %SCRIPT_URL% -UseBasicParsing).Content | Out-File -Encoding UTF8 %TEMP_SCRIPT_PATH%"
+python %TEMP_SCRIPT_PATH% %*
+if exist %TEMP_SCRIPT_PATH% (
+    del %TEMP_SCRIPT_PATH%
+)
+goto :eof
--- .bat/shortcuts-i-have-in-system32/yydl.cmd
+++
@@ -1,3 +0,0 @@
-@echo off
-python "C:\WINDOWS\system32\yydl.py" %1
-@REM yydl.py is nothing but,   a-noob-geek-pc-stuff/py/BEST-YOUTUBE-DOWNLOADER.py   only...renamed for easy recall in cmd 
